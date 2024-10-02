0cbe120424951e68f2d9cb74ed4d93fcfa0f63c0
Quidé <quideclement@gmail.com>
2024-10-02 16:29:49 +0200

    2ème commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var n1 
-n1 = prompt("Saisissez un nombre");
-alert("La conversion de degrés Celsius à degrés Fahrenheit donne : " + (n1*9/5+32));
-console.log("texte affiché dans la console");
+var nb
+nb = prompt("Saisissez un nombre :")
+if (nb%2==0) {
+    alert("Ce nombre est pair.")
+}
+else {
+    alert("Ce nombre est impair.")
+}
