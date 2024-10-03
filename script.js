e02861f661273cace60912dda7308fc18bcd7ef9
Quidé <quideclement@gmail.com>
2024-10-03 11:30:28 +0200

    3ème exercice (non fini)

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-var age
-age = prompt("Saisissez votre âge :")
-if (age>=18) {
-    alert("Vous avez " + age + " ans et êtes donc majeur")
+var nb1;
+var nb2;
+var op;
+var resultat;
+nb1 = prompt("Saisissez un nombre :");
+nb2 = prompt("Saisissez un deuxième nombre :");
+op = prompt("Saisissez un opérateur (+, -, * ou /) :");
+if (op != "+" && op != "-" && op != "*" && op != "/"); {
+    alert("Erreur : opérateur invalide");
 }
 else {
-    alert("Vous avez " + age + " ans et êtes donc mineur")
+    if (op=="+") {
+        resultat = nb1+nb2;
+    }
+    else if (op=="-") {
+        resultat = nb-nb2;
+    }
+    else if (op=="*") {
+        resultat = nb1*nb2;
+    }
+    else if (op=="/") {
+        if (nb2==0) {
+            alert("Erreur : division par zéro");
+        }
+        else {
+            resultat = nb1/nb2;
+        }
+
+    }
+if (resultat !== 0) {
+    alert("Le résultat est de :", resultat)
+}
 }
